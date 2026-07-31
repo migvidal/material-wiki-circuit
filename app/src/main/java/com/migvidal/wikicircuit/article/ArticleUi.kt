38aa952aba7d76f0942031615a448a1ca37d508b
migvidal <miguelvidaf@outlook.com>
2026-07-31 20:35:38 +0200

    Partial: parallax-scrollable main image

--- app/src/main/java/com/migvidal/wikicircuit/article/ArticleUi.kt
+++ app/src/main/java/com/migvidal/wikicircuit/article/ArticleUi.kt
@@ -1,18 +1,12 @@
 package com.migvidal.wikicircuit.article
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
@@ -26,11 +20,9 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.consumePositionChange
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.coerceIn
 import androidx.compose.ui.unit.dp
 import com.migvidal.wikicircuit.R
 import com.migvidal.wikicircuit.core.ui.RequestStatus
@@ -57,7 +49,7 @@ fun ArticleUi(state: ArticleScreen.State, modifier: Modifier = Modifier) {
             val response = state.response
             val status = response.status
             when {
-                !connected ->Text(text = stringResource(R.string.no_internet))
+                !connected -> Text(text = stringResource(R.string.no_internet))
                 status is RequestStatus.Failure -> Text(text = status.message)
                 else -> ArticleBody(state = state)
             }
@@ -70,7 +62,17 @@ private fun SharedElementTransitionScope.ArticleBody(
     state: ArticleScreen.State,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    Column(modifier = modifier.pointerInput(Unit) {
+        awaitEachGesture {
+            do {
+                val event = awaitPointerEvent()
+                event.changes.firstOrNull { it.pressed }?.let {
+                    offsetY = it.position.y
+                }
+            } while (event.changes.any { it.pressed })
+        }
+    }) {
         val response = state.response
         val status = response.status
 
@@ -114,23 +116,27 @@ private fun SharedElementTransitionScope.ArticleBody(
         val mainImg = state.mainImage
 
         val gridState = rememberLazyStaggeredGridState()
-        var offsetX by remember { mutableFloatStateOf(0f) }
+
+        val mainImgHeight by animateFloatAsState(
+            run {
+                val max = if (gridState.canScrollBackward) 50f else 280f
+                offsetY.coerceIn(
+                    minimumValue = 0f,
+                    maximumValue = max,
+                )
+            })
+
 
         CustomAsyncImage(
-            modifier = Modifier.customSharedElement(
-                scope = this@ArticleBody,
-                key = SharedElementKey(type = SharedElementKey.Type.Image, id = mainImg?.source)
-            ).pointerInput(Unit) {
-                detectVerticalDragGestures { change, dragAmount ->
-                    offsetX=dragAmount
-                    change.consume()
-                }
-            },
+            modifier = Modifier
+                .heightIn(max = mainImgHeight.dp)
+                .customSharedElement(
+                    scope = this@ArticleBody,
+                    key = SharedElementKey(type = SharedElementKey.Type.Image, id = mainImg?.source)
+                ),
             imageOrNull = mainImg,
             isDataLoading = isLoading,
         )
-
-        Text(offsetX.toString())
 
         val images = article?.query?.allImages
         LazyVerticalStaggeredGrid(
